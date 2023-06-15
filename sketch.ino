007b3751524ac7e91af26e461de2e0131015c25b
SubhasreeB2418 <128454277+SubhasreeB2418@users.noreply.github.com>
2023-06-15 16:26:15 +0530

    sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -1,10 +1,8 @@
-void setup() {
-  // put your setup code here, to run once:
+void setup(){
 pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 digitalWrite(LED_BUILTIN,HIGH);
 delay(1000);
 digitalWrite(LED_BUILTIN,LOW);
